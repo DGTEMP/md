eb9574ef48682b4183fd633c678c5870bb68892c
DGTEMP <147879523+DGTEMP@users.noreply.github.com>
2024-03-11 19:20:55 -0300

    Update owner-chatgp.js

--- plugins/owner-chatgp.js
+++ plugins/owner-chatgp.js
@@ -10,16 +10,16 @@ const handler = async (m, {conn, text, groupMetadata}) => {
     return !0;
   }
   if (!m.isGroup) return !1;
-  if (!text) throw '*_⚠ • ️Ingrese un -texto- para enviar un mensaje a todos los grupos._*';
+  if (!text) throw '*_⚠ • ️Escreva um -texto- para enviar uma mensagem a todos os grupos._*';
   const linkThisGroup = `${link}`;
-  if (m.text.includes(linkThisGroup)) return conn.reply(m.chat, '❌ *_No puedes espamear enlaces a otros grupos._*', m);
+  if (m.text.includes(linkThisGroup)) return conn.reply(m.chat, '❌ *_Não podes espamar links para outros grupos._*', m);
   const time = global.db.data.users[m.sender].msgwait + 300000;
-  if (new Date - db.data.users[m.sender].msgwait < 300000) throw `*_⚠️ • Tienes que esperar ${msToTime(time - new Date())} para volver a enviar un mensaje._*`;
+  if (new Date - db.data.users[m.sender].msgwait < 300000) throw `*_⚠️ • Tens que esperar ${msToTime(time - new Date())} para voltar a enviar mensagens._*`;
   const who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
   const name = await conn.getName(m.sender);
   const groups = Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats && !chat.metadata?.read_only && !chat.metadata?.announce).map((v) => v[0]);
   const fakegif = {key: {participant: `0@s.whatsapp.net`, ...('6289643739077-1613049930@g.us' ? {remoteJid: '6289643739077-1613049930@g.us'} : {})}, message: {'videoMessage': {'title': '🐱⸽⃕NʏᴀɴCᴀᴛBᴏᴛ - MD🍁⃨፝⃕✰', 'h': `Hmm`, 'seconds': '99999', 'gifPlayback': 'true', 'caption': '🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮', 'jpegThumbnail': false}}};
-  const teks = `*🌺 • 𝙶𝚛𝚞𝚙𝚘:* ${groupMetadata.subject}\n*🍀 • 𝙳𝚎:* ${name}\n*🍁 • 𝙽𝚞́𝚖𝚎𝚛𝚘:* wa.me/${who.split`@`[0]}\n*📧 • 𝙼𝚎𝚗𝚜𝚊𝚓𝚎:* ${text}`;
+  const teks = `*🌺 • 𝙶𝚛𝚞𝚙𝚘:* ${groupMetadata.subject}\n*🍀 • 𝙳𝚎:* ${name}\n*🍁 • 𝙽𝚞́𝚖𝚎𝚛𝚘:* wa.me/${who.split`@`[0]}\n*📧 • 𝙼𝚎𝚗𝚜𝚊g𝚎m:* ${text}`;
   for (const id of groups) {
     await conn.sendMessage(id, {text: teks}, {quoted: fakegif});
     global.db.data.users[m.sender].msgwait = new Date * 1;
