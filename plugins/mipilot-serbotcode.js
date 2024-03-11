08df7bbde65e7471b277809bd6e3b9d056a616e3
DGTEMP <147879523+DGTEMP@users.noreply.github.com>
2024-03-11 19:09:30 -0300

    Update mipilot-serbotcode.js

--- plugins/mipilot-serbotcode.js
+++ plugins/mipilot-serbotcode.js
@@ -3,10 +3,10 @@ async function handler(m, {usedPrefix}) {
     const user = m.sender.split("@")[0]
     if (fs.existsSync("./jadibts/" + user + "/creds.json")) {
         let token = Buffer.from(fs.readFileSync("./jadibts/" + user + "/creds.json"), "utf-8").toString("base64")
-        await m.reply(`El token te permite iniciar sesion en otros bots, recomendamos no compartirlo con nadie.\n\n*Tu token es:*`)
+        await m.reply(`Token te permite iniciar sesion em outros bots.\n\n*Teu token é:*`)
         await m.reply(token)
     } else {
-        await m.reply(`*No tienes ningun token activo, usa ${usedPrefix}jadibot para crear uno.*`)
+        await m.reply(`*Não tens token ativo, use ${usedPrefix}serbot para criar um.*`)
     }
   }
   handler.command = handler.help = ['token', 'gettoken', 'serbottoken'];
