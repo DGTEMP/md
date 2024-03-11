86f0fb66cd11ceaee04d178ab84d2847210fe448
DGTEMP <147879523+DGTEMP@users.noreply.github.com>
2024-03-11 19:29:38 -0300

    Update owner-unbanuser.js

--- plugins/owner-unbanuser.js
+++ plugins/owner-unbanuser.js
@@ -1,12 +1,12 @@
 const handler = async (m, {conn, text}) => {
-  if (!text) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙰 𝙴𝙻 @𝚝𝚊𝚐 𝙳𝙴 𝙰𝙻𝙶𝚄𝙽 𝚄𝚂𝚄𝙰𝚁𝙸𝙾*';
+  if (!text) throw '*[❗𝐈𝐍𝐅𝐎❗]  @𝚝𝚊𝚐 𝙳𝙴 𝚄𝚂𝚄𝙰𝚁𝙸𝙾*';
   let who;
   if (m.isGroup) who = m.mentionedJid[0];
   else who = m.chat;
-  if (!who) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙰 𝙴𝙻 @𝚝𝚊𝚐 𝙳𝙴 𝙰𝙻𝙶𝚄𝙽 𝚄𝚂𝚄𝙰𝚁𝙸𝙾*';
+  if (!who) throw '*[❗𝐈𝐍𝐅𝐎❗] @𝚝𝚊𝚐 𝙳𝙴 𝚄𝚂𝚄𝙰𝚁𝙸𝙾*';
   const users = global.db.data.users;
   users[who].banned = false;
-  conn.reply(m.chat, `*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙵𝚄𝙴 𝙳𝙴𝚂𝙱𝙰𝙽𝙴𝙰𝙳𝙾 𝙲𝙾𝙽 𝙴𝚇𝙸𝚃𝙾*\n*—◉ 𝙴𝙻 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝚈𝙰 𝙿𝚄𝙴𝙳𝙴 𝚄𝚂𝙰𝚁 𝙴𝙻 𝙱𝙾𝚃*`, m);
+  conn.reply(m.chat, `*[❗𝐈𝐍𝐅𝐎❗] USUÁRIO DESBANIDO COM ÊXITO*\n*—◉ JÁ PODE USAR O BOT*`, m);
 };
 handler.help = ['unbanuser'];
 handler.tags = ['owner'];
